9e3ab375401dfb1c4cf791b296c3a6b705e32c47
Ben Hopkins <bjhops17@gmail.com>
2021-09-20 08:24:18 -0700

    added stuff

--- client/src/pages/ViewHouse/index.js
+++ client/src/pages/ViewHouse/index.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react'
 import {useApolloClient} from "@apollo/client"
+import "./style.css"
 
 export default function ViewHouse() {
     const client = useApolloClient();
@@ -9,12 +10,21 @@ export default function ViewHouse() {
     }, [])
     const info = JSON.parse(localStorage.getItem("data"))
     // console.log(client.cache.data.data.ROOT_QUERY.results.array[window.location.pathname[window.location.pathname.length -1]])
-    console.log(info.data.content.list[window.location.pathname[window.location.pathname.length -1]])
+   let house = info.data.content.list[window.location.pathname[window.location.pathname.length -1]]
 
   
     return (
-        <div>
-            HEHEHE
-        </div>
+        <div id="houseContain">
+            <h1>{house.name}</h1>
+            <div>
+                <img src={house.picture_url}></img>
+            </div>
+    <p>{house.description}</p>
+    <h3>Amenities</h3>
+   
+    <ul style={{display: "flex", flexWrap: "wrap", listStyleType: "none"}}>{house.amenities.map(item => <li style={{margin: "10px"}}>{item}</li>)}</ul>  
+    <h3>House Rules</h3>
+    <p>{house.house_rules}</p>
+    </div>
     )
 }
